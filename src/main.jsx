0fb9dadd14661f688d9d5776e578642c96146911
akshatverma1 <akshaverma14@gmail.com>
2025-03-09 18:15:31 +0530

    Testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Homepage from './Components/Homepage.jsx'
-
+import { BrowserRouter , Router, Route, Routes } from 'react-router-dom'
+import Testing from './testing'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Homepage></Homepage>
-  </StrictMode>,
+
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Testing></Testing>} />
+  </Routes>
+  </BrowserRouter>
 )
---
+++ src/testing.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Testing() {
+    return (
+        <div>
+            <h1>Testing</h1>
+            <p>This is a test component</p>
+        </div>
+    );
+}
