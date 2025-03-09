3e55b89f9c5fcc2c3938c4812e3f85113bb9192f
akshatverma1 <akshaverma14@gmail.com>
2025-03-09 19:01:29 +0530

    Testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter , Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter , Route, Routes } from 'react-router-dom'
 import Testing from './testing'
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
   <Routes>
-    <Route path="/" element={<Testing></Testing>} />
+    <Route path="/testing" element={<Testing></Testing>} />
   </Routes>
   </BrowserRouter>
 )
