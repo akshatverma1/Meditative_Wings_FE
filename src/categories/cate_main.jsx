ac50c381e48a88550ba1e0f2edab3790da2b839e
akshatverma1 <akshaverma14@gmail.com>
2025-03-10 21:51:52 +0530

    Frontend Done

--- src/categories/cate_main.jsx
+++ src/categories/cate_main.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import { useState,useEffect } from 'react';
 export default function cate_main(){
+    let url = "http://localhost:3000/allcategories";
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const apiCalling = async () => {
+            try {
+                const response = await fetch(url);
+                const apidata = await response.json();
+                setData(apidata);
+                console.log(apidata);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        apiCalling();
+    }, []);
+   
     return (
         <div>
-            <h1>Categories</h1>
+            {data.map((item)=>{
+                return(
+                    <div>
+                        <h1>{item.videoTitle}</h1>
+                        {/* <p>{item.category_description}</p> */}
+
+                    </div>
+                )
+            })}
         </div>
     );
 }
